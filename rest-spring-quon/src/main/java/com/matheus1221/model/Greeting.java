608ab356e96e8b73e0d2a55bc579cc21f20f2fdd
Matheus1221 <tritanius1221@gmail.com>
2025-08-29 03:28:17 -0300

    first end-point

---
+++ rest-spring-quon/src/main/java/com/matheus1221/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.matheus1221.model;
+
+public record Greeting(long id, String content) {}
---
+++ rest-spring-quon/src/test/java/com/matheus1221/StartupTests.java
@@ -0,0 +1,13 @@
+package com.matheus1221;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
