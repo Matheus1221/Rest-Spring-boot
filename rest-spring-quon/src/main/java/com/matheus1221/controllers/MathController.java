62f5a6be61051cea88707c47e1ccdc6708e93484
Matheus1221 <tritanius1221@gmail.com>
2025-08-31 23:44:10 -0300

    Test RequestVariable

---
+++ rest-spring-quon/src/main/java/com/matheus1221/controllers/MathController.java
@@ -0,0 +1,25 @@
+package com.matheus1221.controllers;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+                @RequestMapping("/sum/{numberOne}/{numberTwo}")
+                public Double sum(
+
+                        @PathVariable("numberOne") String numberOne,
+                        @PathVariable("numberTwo") String numberTwo
+                ){
+                    return 1D;
+
+
+                }
+
+
+
+}
